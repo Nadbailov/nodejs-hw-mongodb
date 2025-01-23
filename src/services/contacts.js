878540cdb266d7fd5a37a537aef6b4742f5f94db
Nadbailov <nadbailov@gmail.com>
2025-01-23 15:07:36 +0200

    2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,6 +3,7 @@ import { ContactCollection } from '../db/models/Contact.js';
 export const getContact = async () => {
   return await ContactCollection.find();
 };
+
 export const getContactById  = async (productId) => {
   return await ContactCollection.findById(productId);
 };
